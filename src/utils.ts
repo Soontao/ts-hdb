6fd90f882ae5e36d94bd7326e794fe65cbd78ee7
Theo Sun <theo.sun@outlook.com>
2023-04-26 16:37:43 +0800

    feat: async generator

--- src/utils.ts
+++ src/utils.ts
@@ -1,33 +1,13 @@
-import { Mutex } from "@newdash/newdash/functional/Mutex";
 import { ResultSet } from "./ResultSet";
 
-export const createAsyncStream = <T>(
-  context: any, 
+export async function* createAsyncStream<T>(
+  context: any,
   streamMethod: "createObjectStream" | "createArrayStream",
   params: any
-): AsyncIterable<T> => {
-  let stream = undefined;
-  let rs: ResultSet<T> = undefined;
-  const mutex = new Mutex();
-  return {
-    [Symbol.asyncIterator]() {
-      return {
-        async next() {
-          if (stream === undefined) {
-            const release = await mutex.acquire();
-            if (stream === undefined) {
-              rs = await context.streamQuery(...params);
-              stream = rs[streamMethod]();
-            }
-            release();
-          }
-          const rt = await stream[Symbol.asyncIterator]().next();
-          if (rt.done === true) {
-            rs.close();
-          }
-          return rt;
-        },
-      };
-    }
-  };
+): AsyncIterable<T> {
+  const rs: ResultSet<T> = await context.streamQuery(...params);
+  const stream = rs[streamMethod]();
+  for await (const rt of stream) {
+    yield rt as any;
+  }
 };
